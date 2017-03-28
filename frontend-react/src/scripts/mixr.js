0b9e2ae2ad34ad2d888ff1cbbe4a779476a7e760
Welby Seely <welby_seely@choice_hotels.com>
2017-03-27 20:05:08 -0400

    hello world react

---
+++ frontend-react/src/scripts/mixr.js
@@ -0,0 +1,7 @@
+/**
+ * Copy pasta'd from https://gist.github.com/danawoodman/9cfddb1a0c934a35f31a
+ */
+(function() {
+  "use strict";
+  ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+}());
