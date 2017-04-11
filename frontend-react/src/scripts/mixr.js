bbed5a1496f744da187657a5eb021fa2b07755fc
cameron_kulhanek <cameron_kulhanek@choicehotels.com>
2017-04-11 18:14:23 -0400

    issue with ingredients

--- frontend-react/src/scripts/mixr.js
+++ frontend-react/src/scripts/mixr.js
@@ -1,9 +1,15 @@
 var Ingredient = React.createClass({
 
 render: function(){
-    var checked = this.props.checked;
+
+
+    var id = this.props.id;
     var name = this.props.name;
-    if(checked === "true"){
+    var description = this.props.description;
+    var ingredientType = this.props.type; //May not be needed here
+    var selected = this.props.selected;
+
+    if(selected === "true"){
     return <tr><h3>{name}<input type="checkbox" checked /> </h3> </tr>
     }else{
      return  <tr><h3>{name}<input type="checkbox" /> </h3> </tr>
@@ -12,22 +18,38 @@ render: function(){
 }
 })
 
-var IngredientsSidebar = React.createClass({
-
-
+var IngredientCategory = React.createClass({
   render: function () {
+  var categoryName = this.props.categoryName;
+  var ingredients = this.props.ingredients;
 
-  var ingredients = [<Ingredient name = "rum" checked="true"/>, <Ingredient name = "vodka" checked="false"/>, <Ingredient name = "whiskey"  checked="true"/>];
-  var ingredient = new Ingredient();
+  var ingredientsObjects = ingredients.map(function (ingredient, index) {
+      return (
+       <Ingredient
+          id={ingredient.id}
+          name={ingredient.name}
+          description={ingredient.description}
+          ingredientType={ingredient.ingredientType}
+          checked="false"
+        />
+      );
+    });
 
-    return<table>{ingredients}</table>
+
+//    return<table>{ingredientsObjects}</table>
+<h1>hello</h1>
   }
-});
+
+})
+
 
 /**
  * Copy pasta'd from https://gist.github.com/danawoodman/9cfddb1a0c934a35f31a
  */
 (function() {
   "use strict";
-  ReactDOM.render(<IngredientsSidebar />, document.getElementById('root'));
+
+  var ingredients = [{"id":"1", "name":"rum", "description":"rum description", "ingredientType":"liquor"},{"id":"2", "name":"vodka", "description":"vodka description", "ingredientType":"liquor"}];
+  ReactDOM.render(<IngredientCategory categoryName="liquor" ingredients={ingredients} />, document.getElementById('root'));
+
 }());
