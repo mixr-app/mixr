9c5105eeb98a7f7a55c0c431a806d2cbcef0afe4
cameron <cameron_kulhanek@choicehotels.com>
2017-04-11 08:16:01 -0400

    added ingredients

--- frontend-react/src/scripts/mixr.js
+++ frontend-react/src/scripts/mixr.js
@@ -1,7 +1,33 @@
+var Ingredient = React.createClass({
+
+render: function(){
+    var checked = this.props.checked;
+    var name = this.props.name;
+    if(checked === "true"){
+    return <tr><h3>{name}<input type="checkbox" checked /> </h3> </tr>
+    }else{
+     return  <tr><h3>{name}<input type="checkbox" /> </h3> </tr>
+     }
+
+}
+})
+
+var IngredientsSidebar = React.createClass({
+
+
+  render: function () {
+
+  var ingredients = [<Ingredient name = "rum" checked="true"/>, <Ingredient name = "vodka" checked="false"/>, <Ingredient name = "whiskey"  checked="true"/>];
+  var ingredient = new Ingredient();
+
+    return<table>{ingredients}</table>
+  }
+});
+
 /**
  * Copy pasta'd from https://gist.github.com/danawoodman/9cfddb1a0c934a35f31a
  */
 (function() {
   "use strict";
-  ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+  ReactDOM.render(<IngredientsSidebar />, document.getElementById('root'));
 }());
